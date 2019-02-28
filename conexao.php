3fa3b9d30ba584c063b148faf3506a2965e410b9
Janilza <jasimao@ua.pt>
2019-02-28 01:47:17 +0000

    Update and rename Database.php to conexao.php

--- conexao.php
+++ conexao.php
@@ -4,7 +4,7 @@ header('Content-Type: text/html; charset=utf-8');
 
 # Conecta ao banco de dados
 // or die - Equivalente/abreviatura a exit() || imprime uma mensagem e sai do script atual
-$conectar = mysql_connect("q7cxv1zwcdlw7699.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","g555luqjip1sjv1a","ly2zjwy3gtwut8b4") or die ("Erro na conexão");
+$conectar = mysql_connect("q7cxv1zwcdlw7699.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","g555luqjip1sjv1a","") or die ("Erro na conexão");
 mysql_select_db("sff7jou3nc27mma2")or die ("Base de dados não encontrada");
 
 # Codificacao 
